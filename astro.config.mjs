dffc860a0bcf0bd2777207124055a01e0004cee7
Joshua Booth <me@joshuabooth.nz>
2022-10-17 16:43:40 +1300

    🔧 vite, image and mdx config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,44 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 import { SITE } from "./src/config.mjs";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
-  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  site: SITE.domain,
+	// Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+	site: SITE.domain,
+	base: SITE.basePathname,
 
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image(),
-    react(),
-  ],
+	output: "static",
+
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		sitemap(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		mdx(),
+		react(),
+	],
+	vite: {
+		resolve: {
+			alias: {
+				"~": path.resolve(__dirname, "./src"),
+			},
+		},
+	},
 });
