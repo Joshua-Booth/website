bc09d7b16a12e5f57470c461a6231b8bd1369acc
Joshua Booth <me@joshuabooth.nz>
2022-10-17 13:07:09 +1300

    🔧 create separate blog config

--- src/config.mjs
+++ src/config.mjs
@@ -2,6 +2,8 @@ export const SITE = {
   name: "Joshua Booth",
   domain: "https://joshua-booth-astro.netlify.app/",
   github: "https://github.com/Joshua-Booth/astro-site",
+};
 
+export const BLOG = {
   postsPerPage: 6,
 };
