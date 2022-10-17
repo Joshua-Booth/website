bab0b05eed030c8ec7c45c8ef7cd90ea2e0f4d2d
Joshua Booth <me@joshuabooth.nz>
2022-10-17 16:38:44 +1300

    ♻️ 🐛 rss and blog refactor and fixing

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,29 @@
 import rss from "@astrojs/rss";
 
-import { SITE } from "@config";
-import { getPosts } from "@utils/getPosts";
+import { SITE, BLOG } from "~/config.mjs";
+import { fetchPosts } from "~/utils/posts";
+import { getPermalink } from "~/utils/permalinks";
 
-const posts = await getPosts();
+export const get = async () => {
+	if (BLOG.disabled) {
+		return new Response(null, {
+			status: 404,
+			statusText: "Not found",
+		});
+	}
 
-export const get = () =>
-  rss({
-    title: `${SITE.name}’s Blog`,
-    description:
-      "A ready to start template to make your website using Astro and Tailwind CSS.",
-    site: import.meta.env.SITE,
-    items: posts.map((post) => ({
-      link: `blog/${post.slug}`,
-      title: post.title,
-      description: post.description,
-      pubDate: post.pubDate,
-    })),
-  });
+	const posts = await fetchPosts();
+
+	return rss({
+		title: `${SITE.name}’s Blog`,
+		description: SITE.description,
+		site: import.meta.env.SITE,
+
+		items: posts.map((post) => ({
+			link: getPermalink(post.slug, "post"),
+			title: post.title,
+			description: post.description,
+			publishDate: post.publishDate,
+		})),
+	});
+};
