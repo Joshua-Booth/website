4bdc5bbec216ed8ed1daf2b2c31f04d9d595922d
Joshua Booth <me@joshuabooth.nz>
2022-11-07 15:37:31 +1300

    ✨ new dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,18 @@ module.exports = {
       colors: {
         primary: colors.blue,
         secondary: colors.pink,
+        gray: {
+          50: "#ECEDEF",
+          100: "#D6D8DC",
+          200: "#ADB0B8",
+          300: "#838995",
+          400: "#5E636E",
+          500: "#3C3F46",
+          600: "#2F3137",
+          700: "#232529",
+          800: "#18191B",
+          900: "#0C0C0E",
+        },
       },
       fontFamily: {
         sans: ["'Albert Sans'", ...defaultTheme.fontFamily.sans],
