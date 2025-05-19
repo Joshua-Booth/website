55586a543311d7540cc55d19550179043e6c4d48
Joshua Booth <joshua@solvedata.io>
2025-05-19 20:53:42 +1200

    🔧 migrate to Astro's built-in font system

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +20,15 @@ export default defineConfig({
     ssr: {
       noExternal: ["@radix-ui/themes"],
     },
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Albert Sans",
+        cssVariable: "--font-albert-sans",
+        weights: ["500", "600", "700", "800"],
+      },
+    ],
   },
 });
